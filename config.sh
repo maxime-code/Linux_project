4f6aa350743285e28850c6f23714bf14bbe3f408
root <root@LAPTOP-D4SOPS2O.localdomain>
2023-05-27 15:13:59 +0200

    ajout du config file et scriptNextcloud!

---
+++ config.sh
@@ -0,0 +1,8 @@
+# Description: Configuration file for the script
+log="mroque25"
+ip="10.30.48.100"
+pathtokey="/home/isen/.ssh/vmisen"
+
+#log="aaugus25"
+#serv="$ip"
+#mdp="Nagatoro25#"
---
+++ scriptNexcloud.sh
@@ -0,0 +1,18 @@
+source config.sh
+
+num_lines=$(wc -l < accounts.csv)
+values1=($(cut -d ';' -f 1 accounts.csv | sed 's/ //g'))
+values2=($(cut -d ';' -f 2 accounts.csv | sed 's/ //g'))
+values3=($(cut -d ';' -f 3 accounts.csv | sed 's/ //g'))
+values4=($(cut -d ';' -f 4 accounts.csv | sed 's/ //g'))
+for (( i=1; i<$num_lines; i++ )); do
+
+    pass="${values4[$i]}"
+    first="${values1[$i]:0:1}"
+	username="$first${values2[$i]}" 
+
+    # ssh -i $pathtokey $log@$ip "export OC_PASS="$pass""
+    # ssh -i $pathtokey $log@$ip "occ user:add --password-from-env --display-name="$username" $username"
+    export OC_PASS="$pass"
+    occ user:add --password-from-env --display-name="$username" $username
+done
