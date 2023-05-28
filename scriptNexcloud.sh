6615b56386291d0a689db558393d9a4417d995d8
Maxime Roquelle <71320650+maxime-code@users.noreply.github.com>
2023-05-28 21:17:23 +0200

    final commit nextcloud.sh

--- scriptNexcloud.sh
+++ scriptNexcloud.sh
@@ -11,8 +11,6 @@ for (( i=1; i<$num_lines; i++ )); do
     first="${values1[$i]:0:1}"
 	username="$first${values2[$i]}" 
 
-    # ssh -i $pathtokey $log@$ip "export OC_PASS="$pass""
-    # ssh -i $pathtokey $log@$ip "occ user:add --password-from-env --display-name="$username" $username"
-    export OC_PASS="$pass"
-    occ user:add --password-from-env --display-name="$username" $username
+    ssh -i $pathtokey $log@$ip "export OC_PASS="$pass""
+    ssh -i $pathtokey $log@$ip "occ user:add --password-from-env --display-name="$username" $username"
 done
